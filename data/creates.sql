2cdaca8dbabfe7d119e6afe9820f2d73ae659c44
Vir19 <viir19@hotmail.com>
2024-02-29 20:58:46 +0100

    files

--- data/creates.sql
+++ data/creates.sql
@@ -10,7 +10,7 @@ CREATE TABLE `netflix`.`movies` (
   `year` INT NULL,
   PRIMARY KEY (`idmovies`));
 
-  INSERT INTO `netflix`.`movies` (`idmovies`, `title`, `genre`, `image`, `category`, `year`) VALUES ('1', 'Pulp Fiction', 'Crimen', 'https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg', 'Top 10', '1994');
+INSERT INTO `netflix`.`movies` (`idmovies`, `title`, `genre`, `image`, `category`, `year`) VALUES ('1', 'Pulp Fiction', 'Crimen', 'https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg', 'Top 10', '1994');
 INSERT INTO `netflix`.`movies` (`idmovies`, `title`, `genre`, `image`, `category`, `year`) VALUES ('2', 'La vita e bella', 'Comedia', 'https://pics.filmaffinity.com/la_vita_e_bella-646167341-mmed.jpg', 'Top 10', '1996');
 INSERT INTO `netflix`.`movies` (`idmovies`, `title`, `genre`, `image`, `category`, `year`) VALUES ('3', 'Forrest Gump', 'Comedia', 'https://pics.filmaffinity.com/forrest_gump-212765827-mmed.jpg', 'Top 10', '1994');
 
@@ -25,7 +25,7 @@ CREATE TABLE `netflix`.`users` (
   `plan_details` VARCHAR(45) CHARACTER SET 'armscii8' NOT NULL,
   PRIMARY KEY (`iduser`));
 
-  INSERT INTO `netflix`.`users` (`iduser`, `user`, `password`, `name`, `email`, `plan_details`) VALUES ('1', 'laura_dev', 'laura', 'Laura', 'laura@gmail.com', 'Standard');
+INSERT INTO `netflix`.`users` (`iduser`, `user`, `password`, `name`, `email`, `plan_details`) VALUES ('1', 'laura_dev', 'laura', 'Laura', 'laura@gmail.com', 'Standard');
 INSERT INTO `netflix`.`users` (`iduser`, `user`, `password`, `name`, `email`, `plan_details`) VALUES ('2', 'maria_dev', 'maria', 'Maria', 'maria@gmail.com', 'Standard');
 INSERT INTO `netflix`.`users` (`iduser`, `user`, `password`, `name`, `email`, `plan_details`) VALUES ('3', 'ester_dev', 'ester', 'Ester', 'ester@gmail.com', 'Standard');
 
@@ -39,6 +39,6 @@ CREATE TABLE `netflix`.`actors` (
   `birthday` DATE NULL,
   PRIMARY KEY (`idactors`));
 
-  INSERT INTO `netflix`.`actors` (`idactors`, `name`, `lastname`, `country`, `birthday`) VALUES ('1', 'Tom', 'Hanks', 'Estados Unidos', '1956-07-09');
+INSERT INTO `netflix`.`actors` (`idactors`, `name`, `lastname`, `country`, `birthday`) VALUES ('1', 'Tom', 'Hanks', 'Estados Unidos', '1956-07-09');
 INSERT INTO `netflix`.`actors` (`idactors`, `name`, `lastname`, `country`, `birthday`) VALUES ('2', 'Roberto', 'Benigni', 'Italia', '1952-10-27');
 INSERT INTO `netflix`.`actors` (`idactors`, `name`, `lastname`, `country`, `birthday`) VALUES ('3', 'John', 'Travolta', 'Estados Unidos', '1954-02-18');
